ca1fdc81b94fc20cadc1ca972053ae1d88933474
Sithil-S <sithilsandinu123@gmail.com>
2024-02-25 20:25:35 +0530

    splashscreen new

--- lib/pages/Home/home.dart
+++ lib/pages/Home/home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+
+import 'package:wildlifeconnect/pages/Home/components/nav_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:wildlifeconnect/pages/Home/components/camera_page.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,6 +13,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+          ),
+        ],
+
         backgroundColor: Colors.green,
         leading: IconButton(
           icon: const Icon(Icons.camera_alt),
@@ -25,6 +39,7 @@ class HomePage extends StatelessWidget {
           ),
         ),
         centerTitle: true,
+
       ),
       body: Container(
         decoration: const BoxDecoration(
@@ -41,6 +56,7 @@ class HomePage extends StatelessWidget {
               child: ListView.builder(
                 itemCount: 7,
                 itemBuilder: (context, index) {
+                  // Example image URLs (replace with your actual image URLs)
                   List<String> imageUrls = [
                     'https://i.natgeofe.com/n/b64060fa-343c-481b-a24d-7375fef34914/NationalGeographic_1425689_3x4.jpg',
                     'https://i.natgeofe.com/n/b64060fa-343c-481b-a24d-7375fef34914/NationalGeographic_1425689_3x4.jpg',
