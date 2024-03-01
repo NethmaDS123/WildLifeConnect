16a9aba08e56e567c6b3e5f4bc4b8637e7b5dfc5
NethmaDS123 <dsnethma@gmail.com>
2024-03-01 15:26:51 +0530

    fixed library not working on android issue

--- lib/pages/Library/API/animal_service.dart
+++ lib/pages/Library/API/animal_service.dart
@@ -1,10 +1,24 @@
-// animal_service.dart
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:io' show Platform;
+import 'package:http/http.dart' as http;
 import 'package:wildlifeconnect/pages/Library/API/animalModel.dart';
 
+String getBackendUrl() {
+  if (Platform.isAndroid) {
+    // Use 10.0.2.2 for Android emulator
+    return 'http://10.0.2.2:3000';
+  } else if (Platform.isIOS) {
+    // Use localhost for iOS simulator
+    return 'http://localhost:3000';
+  } else {
+    // Fallback for other platforms
+    return 'http://your-production-url.com';
+  }
+}
+
 Future<List<Animal>> fetchAnimals() async {
-  final response = await http.get(Uri.parse('http://localhost:3000/animals'));
+  var backendUrl = getBackendUrl();
+  final response = await http.get(Uri.parse('$backendUrl/animals'));
 
   if (response.statusCode == 200) {
     List<dynamic> body = jsonDecode(response.body);
