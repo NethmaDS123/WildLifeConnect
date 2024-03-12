0b111f57059673d80ce3d99bbb5f47355bdf88bc
Evin <evinhandapangoda@gmail.com>
2024-03-12 19:04:45 +0530

    profile-functionality-2

--- lib/pages/Profile/pages/profile.dart
+++ lib/pages/Profile/pages/profile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:wildlifeconnect/model/user_test.dart';
+import 'package:wildlifeconnect/pages/Profile/model/post.dart';
 import 'package:wildlifeconnect/pages/Profile/pages/sidebar_widget.dart';
+import 'package:wildlifeconnect/pages/Profile/service/post_service.dart';
 import 'package:wildlifeconnect/pages/Profile/utils/user_preferences.dart';
 import 'package:wildlifeconnect/pages/Profile/widgets/appbar_widget.dart';
 import 'package:wildlifeconnect/pages/Profile/widgets/button_widget.dart';
@@ -16,13 +19,24 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePage1State extends State<ProfilePage> {
+  late Future<List<Post>> postsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    postsFuture = fetchAllPosts();
+    print('active');
+  }
+
+  Future<List<Post>> fetchAllPosts() {
+    return fetchPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     const prof_user = UserPreferences.myUser;
-    const imgUrl =
-        'https://i.natgeofe.com/n/b64060fa-343c-481b-a24d-7375fef34914/NationalGeographic_1425689_3x4.jpg';
 
     return Scaffold(
       backgroundColor: Colors.black,
@@ -52,23 +66,40 @@ class _ProfilePage1State extends State<ProfilePage> {
             child: buildReportButton(),
           ),
           const SizedBox(height: 14),
-          GridView.count(
-            padding: const EdgeInsets.all(5),
-            crossAxisCount: 3,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            children: [
-              PostWidget(height: height, width: width, imgUrl: imgUrl),
-              PostWidget(height: height, width: width, imgUrl: imgUrl),
-              PostWidget(height: height, width: width, imgUrl: imgUrl),
-              PostWidget(height: height, width: width, imgUrl: imgUrl),
-              PostWidget(height: height, width: width, imgUrl: imgUrl),
-              PostWidget(height: height, width: width, imgUrl: imgUrl),
-              PostWidget(height: height, width: width, imgUrl: imgUrl),
-              PostWidget(height: height, width: width, imgUrl: imgUrl),
-            ],
+          FutureBuilder<List<Post>>(
+            future: postsFuture,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final posts =
+                    snapshot.data!; // Safe access after checking hasData
+                return GridView.count(
+                  padding: const EdgeInsets.all(5),
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  children: List.generate(
+                    posts.length,
+                    (index) => PostWidget(
+                      height: height,
+                      width: width,
+                      imgUrl:
+                          posts[index].imgUrl, // Access imgUrl from each Post
+                    ),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                    child: Text(
+                  'Error: ${snapshot.error}',
+                  style: TextStyle(color: Colors.white),
+                ));
+              }
+
+              // Show a loading indicator while waiting for data
+              return const Center(child: CircularProgressIndicator());
+            },
           ),
         ],
       ),
