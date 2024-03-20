05f0aff701a26fab3763cb63352b54901a12462c
Omesh Chandrapala <Omeshrc@users.noreply.github.com>
2024-03-21 03:03:14 +0530

    Updated the Library UI

--- lib/pages/Library/components/search_bar.dart
+++ lib/pages/Library/components/search_bar.dart
@@ -14,25 +14,36 @@ class _SearchBarState extends State<SearchBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: _controller,
-        decoration: InputDecoration(
-          hintText: 'Search for animals...',
-          prefixIcon: const Icon(Icons.search),
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.clear),
-            onPressed: () {
-              _controller.clear();
-              widget.onSearch('');
-            },
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16.0),
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(
+          color: Colors.white, // white colour border of the search bar
+          width: 0.8, // Border width
         ),
-        onChanged: widget.onSearch,
+      ),
+      child: Card(
+        elevation: 10, 
+        child: TextField(
+          controller: _controller,
+          style: const TextStyle(color: Colors.lime), // Text color of the input field text
+          decoration: InputDecoration(
+            hintText: 'Search for animals...',
+            hintStyle: const TextStyle(color: Colors.white), // Placeholder color
+            prefixIcon: const Icon(Icons.search, color: Colors.white), // search icon color
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.clear, color: Colors.white), // reset icon color
+              onPressed: () {
+                _controller.clear();
+                widget.onSearch('');
+              },
+            ),
+            filled: true,
+            fillColor: const Color.fromARGB(255, 33, 33, 33), // Background color
+            border: InputBorder.none, // No border for the input field
+          ),
+          onChanged: widget.onSearch,
+        ),
       ),
     );
   }
