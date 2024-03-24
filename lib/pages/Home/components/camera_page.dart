29a5067b89b4b5212cbbcbc076ce1076b6576a49
Anthony-Emmanuel <129255721+Anthony-Emmanuel@users.noreply.github.com>
2024-03-24 16:20:06 +0530

    identifier ini

--- lib/pages/Home/components/camera_page.dart
+++ lib/pages/Home/components/camera_page.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http_parser/http_parser.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -35,6 +40,46 @@ class _CameraPageState extends State<CameraPage> {
     }
   }
 
+  Future<void> _identifyAnimal(File? _imageFile) async {
+  final uri = Uri.parse('https://wildlifeconnectbackend.onrender.com/identifiers/saveNget');
+
+  const storage = FlutterSecureStorage();
+  String? authToken = await storage.read(key: 'jwt_token');
+
+  // Create a multipart request
+  var request = http.MultipartRequest('POST', uri);
+
+  request.headers.addAll({'Authorization': 'Bearer $authToken'});
+
+  // Add the image file to the request
+  var file = await http.MultipartFile.fromPath(
+    'image',
+    _imageFile!.path,
+    contentType: MediaType('image', 'jpeg'), // Adjust content type if necessary
+  );
+  request.files.add(file);
+
+  try {
+    // Send the request
+    final streamedResponse = await request.send();
+    final response = await http.Response.fromStream(streamedResponse);
+
+    if (response.statusCode == 200) {
+      // Token saved successfully
+      final responseData = json.decode(response.body);
+      String location = responseData['location'];
+      print('Image saved successfully at $location');
+      // Now you can use the 'location' variable as needed.
+    } else {
+      // Error saving token
+      print('Error saving image: ${response.body}');
+    }
+  } catch (e) {
+    // Error handling
+    print('Error saving image: $e');
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,16 +100,25 @@ class _CameraPageState extends State<CameraPage> {
             ),
             const SizedBox(height: 20),
             if (_imageFile != null)
-              Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                  child: Image.file(
+                    _imageFile!,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: Image.file(
-                  _imageFile!,
-                  fit: BoxFit.cover,
+                ElevatedButton(
+                  onPressed: () => _identifyAnimal(_imageFile),
+                  child: const Text('Identify Animal'),
                 ),
-              ),
+              ],
+            )
           ],
         ),
       ),
